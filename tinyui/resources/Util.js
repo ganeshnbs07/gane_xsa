cec7abde3e20cfee4ca16c9cc29848ca78e5d264
ganeshnbs07 <ganeshnbs07@gmail.com>
2021-04-21 20:14:17 +0000

    Final changes of tinyworld project 1

--- tinyui/resources/Util.js
+++ tinyui/resources/Util.js
@@ -1,9 +1,10 @@
 var oFirstDialog;
+
 function openFirstDialog() {
-  if (oFirstDialog) {
-     oFirstDialog.open();
-  } else {
-     oFirstDialog = new sap.ui.commons.Dialog({
+   if (oFirstDialog) {
+      oFirstDialog.open();
+   } else {
+      oFirstDialog = new sap.ui.commons.Dialog({
          width: "400px", // sap.ui.core.CSSSize
          height: "550px", // sap.ui.core.CSSSize
          title: "Country Details", // string
@@ -18,8 +19,9 @@ function openFirstDialog() {
                new sap.ui.commons.TextField({ value: "", id: "partof" })
             ]
          })] // sap.ui.core.Control
-  });
-     oFirstDialog.addButton(new sap.ui.commons.Button({
+      });
+
+      oFirstDialog.addButton(new sap.ui.commons.Button({
          text: "OK",
          press: function() {
             var name = sap.ui.getCore().byId("name").getValue();
@@ -28,48 +30,46 @@ function openFirstDialog() {
             payload.name = name;
             payload.partof = partof;
             var insertdata = JSON.stringify(payload);
-
-
+            
             $.ajax({
-
                type: "POST",
-
                url: "country/country.xsjs",
-
                contentType: "application/json",
-
                data: insertdata,
-
                dataType: "json",
-
                crossDomain: true,
-
                success: function(data) {
-
                   oFirstDialog.close();
-
                   sap.ui.getCore().byId("tinytab").getModel().refresh(true);
-
                   alert("Data inserted successfully");
-
                },
-
                error: function(data) {
-
                   var message = JSON.stringify(data);
-
                   alert(message);
-
                }
-
             });
-
          }
+      }));
 
-   }));
-
-   oFirstDialog.open();
-
+      oFirstDialog.open();
+   }
 }
 
-}
+$(function() {
+  // one time fetch of CSRF token
+  $.ajax({
+    type: "GET",
+    url: "/",
+    headers: {"X-Csrf-Token": "Fetch"},
+    success: function(res, status, xhr) {
+      var sHeaderCsrfToken = "X-Csrf-Token";
+      var sCsrfToken = xhr.getResponseHeader(sHeaderCsrfToken);
+      // for POST, PUT, and DELETE requests, add the CSRF token to the header
+      $(document).ajaxSend(function(event, jqxhr, settings) {
+        if (settings.type==="POST" || settings.type==="PUT" || settings.type==="DELETE") {
+          jqxhr.setRequestHeader(sHeaderCsrfToken, sCsrfToken);
+        }
+      });
+    }
+  });
+});
